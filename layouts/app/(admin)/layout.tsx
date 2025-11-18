606ae486854cd7beabc45f49d733fda8fcf3cd4f
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2025-11-19 00:56:09 +0530

    layout in next.js

---
+++ layouts/app/(admin)/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google"; 
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Admin: Facebook - Connect with friends and the world around you",
+  description: "Admin Page: Facebook is a social utility that connects people with friends and others who work, study and live around them. People use Facebook to keep up with friends, upload an unlimited number of photos, post links and videos, and learn more about the people they meet.",
+};
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+     <>
+     <span>Admin Navbar</span>
+        {children} 
+     </>
+  );
+}
