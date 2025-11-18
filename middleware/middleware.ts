f2bd2e32718e1811eb8f594c8bee7a4d4cc540ec
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2025-11-18 17:50:04 +0530

    Middleware for handling incoming request and response

---
+++ middleware/middleware.ts
@@ -0,0 +1,32 @@
+// import { NextResponse } from 'next/server'
+ 
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request) {
+//     // return NextResponse.json({ message: 'Hello from the about page' })
+//   return NextResponse.redirect(new URL('/', request.url))
+// }
+ 
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: '/about/:path*',
+// }
+
+
+import { NextRequest, NextResponse } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/about')) {
+    return NextResponse.rewrite(new URL('/', request.url))
+  }
+ 
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+   return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/about/:path*", "/dashboard/:path*"],
+  //matcher function says that Only run this middleware for these URL patterns.
+};
